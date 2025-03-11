7db5a366459d98e2d7c97b35766c2d87304a43b1
cocoperra <guiagulop@gmail.com>
2025-03-11 13:37:55 +0100

    Succesfully upgraded my token into my Read function

--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,13 @@
 // Definir la URL base de la API
 const apiUrl = "http://127.0.0.1:8000/api/jugadores/";
 
-
 // Variable global para almacenar el equipo actual
 let equipoActual = "M"; 
+
+// Función para obtener el Token desde Storage
+function getToken() {
+  return sessionStorage.getItem("access_token");
+}
 
 // Cargar los jugadores desde la API con el equipo seleccionado
 async function cargarJugadores(equipo = equipoActual) {
@@ -12,25 +16,14 @@ async function cargarJugadores(equipo = equipoActual) {
   if (equipo) {
     url += `?equipo=${equipo}`; // Agregar parámetro de equipo si se especifica
   }
- //Manually introducing my Token
- 
-  try {
-    // Obtener el token de acceso desde el sessionStorage
-    const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQxNjk1Nzc3LCJpYXQiOjE3NDE2OTU0NzcsImp0aSI6IjU0ZWRlZjQ0ODQ3NTQ0Y2Y4ZGQ0OWQ5OWIxZTk1Nzk5IiwidXNlcl9pZCI6M30.jZ6h7UhU5RzjEIGBeho1mh6j87uRxBmBRfSBRHTT10k';
-    
-    // Si no hay token, mostrar error o redirigir a login
-    if (!accessToken) {
-      console.error("No hay token de acceso. Por favor, inicia sesión.");
-      // Podrías redirigir a la página de login aquí si lo prefieres
-      return;
-    }
 
+  try {
     // Hacer la solicitud GET con el token en el header
     const response = await fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${accessToken}` // Incluir el token de acceso
+        'Authorization': `Bearer ${getToken()}` // Usamos el token almacenado en sessionStorage
       }
     });
 
@@ -64,7 +57,6 @@ async function cargarJugadores(equipo = equipoActual) {
     $("#jugadoresList").html("<li class='list-group-item'>Error al cargar jugadores. Intenta nuevamente.</li>");
   }
 }
-
 
 // Agregar un nuevo jugador
 $("#addJugadorForm").submit(function (event) {
